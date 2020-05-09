fc7cc227f12e504a91acd4da16cc1d73c03f5733
vodpox <edvinas.senda@gmail.com>
2020-05-09 19:27:44 +0300

    authroute logic now supports forcing to change password on first login

--- src/components/AuthRoute.jsx
+++ src/components/AuthRoute.jsx
@@ -12,26 +12,39 @@ class AuthRoute extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			loggedIn: null
+			loggedIn: null,
+			user: null
 		}
 	}
 	
-	componentDidMount() {
-		auth.isLoggedIn()
-			.then((bool) => this.setState({loggedIn: bool}));
+	async componentDidMount() {
+		
+		this.setState({loggedIn: await auth.isLoggedIn()});
+		//this.setState({user: await auth.getUser()});
+		
+		// temporary
+		var usr = await auth.getUser();
+		if (usr !== null) {
+			usr.hasChangedInitalPassword = true;
+			this.setState({user: usr});
+		}
 	}
 	
 	render() {
 		
-		if (this.state.loggedIn === null) {
+		if (this.state.loggedIn === null || (this.state.loggedIn && this.state.user === null)) {
 			return <Loading/>;
 		}
-
-		return (
-			this.state.loggedIn === true ? 
-				<Route component={this.props.component} path={this.props.path}></Route>:
-				<Redirect to="/login" />		
-		);
+		
+		if (!this.state.loggedIn) {
+			return <Redirect to="/login" />;
+		}
+		
+		if (!this.state.user.hasChangedInitalPassword) {
+			return "TODO: password change view";
+		}
+		
+		return <Route component={this.props.component} path={this.props.path}></Route>;
 	}
 	
 }
