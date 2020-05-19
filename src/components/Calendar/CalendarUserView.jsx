2f23daf1c2776c1ecae858cc08ecaad23f3fe98f
Jonas Petraška <jonas.petraska@gmail.com>
2020-05-20 02:08:52 +0300

    Adjustments not to show "Go to this users page" link when current user is looking at his own calendar

--- src/components/Calendar/CalendarUserView.jsx
+++ src/components/Calendar/CalendarUserView.jsx
@@ -120,11 +120,12 @@ class CalendarUserView extends React.Component {
 
 						<div className="cal-main-panel">
 
-							<Link className="margin-bottom-16" to={"/user/" + this.props.match.params.id === "me" ? auth.user.id : this.props.match.params.id}>
-								<FontAwesomeIcon className="margin-right-8" icon={faAngleLeft} />
-								Go to this user's page
-							</Link>
-
+							{this.props.match.params.id !== "me" && this.props.match.params.id !== auth.user.id ?
+								<Link className="margin-bottom-16" to={"/user/" + this.props.match.params.id === "me" ? auth.user.id : this.props.match.params.id}>
+									<FontAwesomeIcon className="margin-right-8" icon={faAngleLeft} />
+									Go to this user's page
+								</Link> : ""
+							}
 							<Calendar
 								localizer={localizer}
 								views={{ month: true }}
