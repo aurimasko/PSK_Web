50a2d27c8f4b48e13a1be078a8a7bacb52518fdf
vodpox <edvinas.senda@gmail.com>
2020-05-19 23:56:00 +0300

    new user page design

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from "./Layout";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faCalendarAlt, faUsers, faTasks, faClipboardCheck } from '@fortawesome/free-solid-svg-icons'
+import { faUnlockAlt, faPen, faUser, faCalendarAlt, faUsers, faTasks, faClipboardCheck } from '@fortawesome/free-solid-svg-icons'
 import { Link } from "react-router-dom";
 import { auth } from "../services/auth.js";
 import { roleService } from "../services/roleService.js";
@@ -72,161 +72,278 @@ class User extends React.Component {
 	}
 	
 	renderRole() {
-		if (this.state.role == null) {
-			return <Loading width={50} height={50} type={"balls"} />;
-		} else {
-			if (this.state.role.id) {
-				return (
-					<h4>
-						Role: <Link className="" to={"/role/" + this.state.role.id}>{this.state.role.name}</Link>
-					</h4>);
-			} else {
-				return "";
-			}
-		}
-	}
-
-	renderButtons() {
-		//Admin can do anything
-		if (auth.user.isAdmin) {
+		
+		if (this.state.role === null) {
 			return (
-				<div>
-					<Link className="button" to={"/user/" + this.state.user.id + "/edit"}>
-						<button>Edit</button>
+				<h4>
+					Role: <span className="inline-block"><Loading width={16} height={16} type={"spin"} /></span>
+				</h4>
+			);
+		}
+		
+		if (this.state.role.id) {
+			return (
+				<h4>
+					Role: <Link className="" to={"/role/" + this.state.role.id}>{this.state.role.name}</Link>
+				</h4>
+			);
+		}
+		else {
+			return (
+				<h4>
+					Role: <span className="unbold">none</span>
+					{this.canChangeRole ? 
+						<Link className="unbold margin-left-24" to={"/user/" + this.state.user.id + "/changerole"}>
+							<FontAwesomeIcon className="margin-right-4" icon={faPen} />
+							edit
+						</Link>:
+						""
+					}
+				</h4>
+			);
+		}
+		
+	}
+	
+	renderSupervisor() {
+		
+		if (this.state.user === null) {
+			return ( 
+				<>
+					<strong>Supervisor: </strong>
+					<span className="inline-block">
+						<Loading width={16} height={16} type={"spin"} />
+					</span>
+				</>
+			);
+		}
+		
+		return (
+			<div>
+				<strong>Supervisor: </strong> TODO
+				{this.canChangeSupervisor() ?
+					<Link className="unbold margin-left-24" to={"/user/" + this.state.user.id + "/changesupervisor"}>
+						<FontAwesomeIcon className="margin-right-4" icon={faPen} />
+						edit
+					</Link>:
+					""
+				}
+			</div>
+		);
+	}
+	
+	canEditUserInfo() {
+		if (this.state.user !== null && (this.state.user.id === auth.user.id)) {
+			return true;
+		}
+		else return false;
+	}
+	
+	canChangePassword() {
+		if (this.state.user !== null && this.state.user.id === auth.user.id) {
+			return true;
+		}
+		else return false;
+	}
+	
+	canChangeRole() {
+		if (this.state.user !== null && (auth.user.isAdmin || this.state.user.superVisorId === auth.user.id)) {
+			return true;
+		}
+		else return false;
+	}
+	
+	canChangeSupervisor() {
+		if (this.state.user !== null && auth.user.isAdmin) {
+			return true;
+		}
+		else return false;
+	}
+	
+	renderMainButtons() {
+		if (this.state.user === null) {
+			return (
+				<div className="grid gaps">
+					<Link className="button disabled">
+						<div className="w100 margin-vertical-16">
+							<FontAwesomeIcon icon={faCalendarAlt} size="3x" />
+						</div>
+						Calendar
 					</Link>
-					<Link className="button" to={"/user/" + this.state.user.id + "/changerole"}>
-						<button>Change role</button>
+					
+					<Link className="button disabled">
+					<div className="w100 margin-vertical-16">
+						<FontAwesomeIcon icon={faTasks} size="3x" />
+					</div>
+						Objectives
 					</Link>
-					<button>Change supervisor</button>
+					
+					<Link className="button disabled">
+					<div className="w100 margin-vertical-16">
+						<FontAwesomeIcon icon={faClipboardCheck} size="3x" />
+					</div>
+						Learned topics
+					</Link>
+					
+					<Link className="button disabled">
+						<div className="w100 margin-vertical-16">
+							<FontAwesomeIcon icon={faUsers} size="3x" />
+						</div>
+						Team
+					</Link>
+					
 				</div>
 			);
-		} else {
-			//if current user, can change password and update himself
-			if (this.state.user.id === auth.user.id) {
-				return (
-					<div>
-						<Link className="button" to={"/user/" + this.state.user.id + "/edit"}>
-							<button>Edit</button>
-						</Link>
-						<Link className="button" to={"/user/me/changepassword"}>
-							<button>Change password</button>
-						</Link>
-					</div>
-				);
-				//if supervisor, can update only role
-			} else if (this.state.user.superVisorId === auth.user.id) {
-				return (
-					<Link className="button" to={"/user/" + this.state.user.id + "/changerole"}>
-						<button>Change role</button>
+		}
+		else {
+			return (
+				<div className="grid gaps">
+					<Link className="button align-left" to={"/user/" + this.state.user.id + "/calendar"}>
+						<FontAwesomeIcon className="margin-right-8" icon={faCalendarAlt} />
+						Calendar
 					</Link>
-				);
-				//if any higher in heirarchy, can update role and change supervisor
-			} else {
-				return (
-					<div>
-						<Link className="button" to={"/user/" + this.state.user.id + "/changerole"}>
-							<button>Change role</button>
-						</Link>
-						<button>Change supervisor</button>
-					</div>
-				);
-			}
+					
+					<Link className="button align-left" to={"/user/" + this.state.user.id + "/objectives"}>
+						<FontAwesomeIcon className="margin-right-8" icon={faTasks} />
+						Objectives
+					</Link>
+					
+					<Link className="button align-left" to={"/user/" + this.state.user.id + "/topics"}>
+						<FontAwesomeIcon className="margin-right-8" icon={faClipboardCheck} />
+						Learned topics
+					</Link>
+					
+					<Link className="button align-left" to={"/user/" + this.state.user.id + "/team"}>
+						<FontAwesomeIcon className="margin-right-8" icon={faUsers} />
+						Team
+					</Link>
+					
+				</div>
+			);
 		}
 	}
-
+	
 	renderAttributes() {
+		
+		if (this.state.user === null) {
+			return "";
+		}
+		
 		if (!this.state.user.superVisorId && this.state.user.isSuperVisor) {
-			return "Admin, Super visor";
+			return (
+				<>
+					<span className="badge">Admin</span>
+					<span className="badge">Supervisor</span>
+				</>
+			);
 		} else if (!this.state.user.superVisorId) {
-			return "Admin";
+			return <span className="badge">Admin</span>;
 		} else if (this.state.user.isSuperVisor) {
-			return "Super visor";
+			return <span className="badge">Supervisor</span>;
 		} else {
-			return "Member";
+			return <span className="badge">Member</span>;
 		}
 	}
-
-	render() {
-		if (this.state.user == null) {
+	
+	renderInfo() {
+		
+		if (this.state.user === null) {
 			return (
-				<Layout ref={this.notifRef}>
-					<Loading showText={true}/>
-				</Layout>
-			);
-		} else {
-			
-			return (
-				<Layout ref={this.notifRef}>
-					<div className="container wide">
-						<div className="flex-right">
-						
-							<div className="flex-down margin-right-16 margin-left-8">
-								<div className="flex-spacer"></div>
-								<FontAwesomeIcon icon={faUser} size="3x" />
-								<div className="flex-spacer"></div>
-							</div>
-							
-							<div>
-								<h1>
-									{this.state.user.firstName} {this.state.user.lastName}
-								</h1>
-								{this.renderAttributes()}
-								{this.renderRole()}
-							</div>
-							
-						</div>
-						
-						<div className="wide width-container">
-							
-							<div className="grid gaps">
-								<Link className="button" to={"/user/" + this.state.user.id + "/calendar"}>
-									<div className="w100 margin-vertical-16">
-										<FontAwesomeIcon icon={faCalendarAlt} size="3x" />
-									</div>
-									Calendar
-								</Link>
-								
-								<Link className="button" to={"/user/" + this.state.user.id + "/objectives"}>
-								<div className="w100 margin-vertical-16">
-									<FontAwesomeIcon icon={faTasks} size="3x" />
-								</div>
-									Objectives
-								</Link>
-								
-								<Link className="button" to={"/user/" + this.state.user.id + "/topics"}>
-								<div className="w100 margin-vertical-16">
-									<FontAwesomeIcon icon={faClipboardCheck} size="3x" />
-								</div>
-									Learned topics
-								</Link>
-								
-								<Link className="button" to={"/user/" + this.state.user.id + "/team"}>
-									<div className="w100 margin-vertical-16">
-										<FontAwesomeIcon icon={faUsers} size="3x" />
-									</div>
-									Team
-								</Link>
-								
-							</div>
-						</div>
-						
-						<div className="margin-top-16">
-							<strong>Email address: </strong> {this.state.user.username}
-						</div>
-						
-						<div>
-							<strong>Registration date: </strong> {moment.utc(this.state.user.creationDate).local().format('YYYY-MM-DD hh:mm')}
-						</div>
-						<div>
-							<strong>Learning day limit per quarter: </strong> {this.state.user.learningDayLimitPerQuarter}
-						</div>
-
-						{this.renderButtons()}
-						
+				<>
+					<div className="margin-top-16">
+						<strong>Email address: </strong> <span className="inline-block"><Loading width={16} height={16} type={"spin"} /></span>
 					</div>
-				</Layout>
+					<div>
+						<strong>Registration date: </strong> <span className="inline-block"><Loading width={16} height={16} type={"spin"} /></span>
+					</div>
+					<div>
+						<strong>Learning day limit per quarter: </strong> <span className="inline-block"><Loading width={16} height={16} type={"spin"} /></span>
+					</div>
+				</>
 			);
 		}
+		else {
+			return (
+				<>
+					<div className="margin-top-16">
+						<strong>Email address: </strong> {this.state.user.username}
+					</div>
+					<div>
+						<strong>Registration date: </strong> {moment.utc(this.state.user.creationDate).local().format('YYYY-MM-DD hh:mm')}
+					</div>
+					<div>
+						<strong>Learning day limit per quarter: </strong> {this.state.user.learningDayLimitPerQuarter}
+					</div>
+				</>
+			);
+		}
+	}
+	
+	render() {
+		return (
+			<Layout ref={this.notifRef}>
+				<div className="container wide">
+					<div className="flex-right">
+					
+						<div className="flex-down margin-right-16 margin-left-8">
+							<div className="flex-spacer"></div>
+							<FontAwesomeIcon icon={faUser} size="3x" />
+							<div className="flex-spacer"></div>
+						</div>
+						
+						<div>
+							{
+								this.state.user !== null ?
+								<div className="flex-right flex-wrap">
+									<h1 className="margin-right-16">
+										{this.state.user.firstName} {this.state.user.lastName}
+									</h1>
+									{this.renderAttributes()}
+								</div> :
+								<h1>
+									Loading user...
+								</h1>
+							}
+							
+							{this.renderSupervisor()}
+							{this.renderRole()}
+						</div>
+						
+					</div>
+					<hr />
+					{this.renderMainButtons()}
+					
+					<hr />
+					
+					<div className="flex-right flex-wrap">
+						<h2>
+							More information:
+						</h2>
+						
+						<div className="flex-spacer" />
+						
+						{this.canEditUserInfo() ?
+							<Link className="unbold margin-right-32 margin-top-8" to={"/user/" + this.state.user.id + "/edit"}>
+								<FontAwesomeIcon className="margin-right-4" icon={faPen} />
+								edit
+							</Link>:
+							""
+						}
+					</div>
+					
+					{this.renderInfo()}
+					
+					{
+						this.canChangePassword() ?
+							<Link className="button border margin-top-32" to={"/user/me/changepassword"}>
+								<FontAwesomeIcon className="margin-right-8" icon={faUnlockAlt} />
+								Change password
+							</Link>:
+							""
+					}
+				</div>
+			</Layout>
+		);
 	}
 }
 
