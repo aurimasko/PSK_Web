c7462c397d9b5f2bbbbae975bfc765041101fbf2
Jonas Petraška <jonas.petraska@gmail.com>
2020-05-20 21:26:12 +0300

    Added not found and roles translations

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,7 @@ import Layout from "./Layout";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { Link } from "react-router-dom";
-
+import { languageService } from "../services/languageService.js";
 
 class NotFound extends React.Component {
 	
@@ -15,9 +15,9 @@ class NotFound extends React.Component {
 					<h1 className="extra center">
 						<FontAwesomeIcon icon={faSearch} />
 					</h1>
-					<h1 className="center">Page not found</h1>
+					<h1 className="center">{languageService.translate("NotFound.Title")}</h1>
 					
-					<Link className="button primary margin-top-32" to="/">Go back to the start screen</Link>
+					<Link className="button primary margin-top-32" to="/">{languageService.translate("NotFound.Link")}</Link>
 				</div>
 				<div className="flex-spacer" />
 			</Layout>
