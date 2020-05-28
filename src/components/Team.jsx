f44dc2f6cc6d3d426d955d677e8a657fcce3115d
vodpox <edvinas.senda@gmail.com>
2020-05-29 00:26:42 +0300

    Added topic planning for teams

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from "./Layout";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUsers, faStar, faUser, faCalendarAlt, faClipboardList, faProjectDiagram } from '@fortawesome/free-solid-svg-icons'
+import { faUsers, faStar, faUser, faCalendarAlt, faClipboardList, faProjectDiagram, faColumns } from '@fortawesome/free-solid-svg-icons'
 import { Link } from "react-router-dom";
 import Loading from "../components/Loading";
 import { teamService } from "../services/teamService.js";
@@ -171,6 +171,13 @@ class Team extends React.Component {
 								<FontAwesomeIcon icon={faProjectDiagram} size="4x" />
 							</div>
 							{languageService.translate("Team.LearningPath")}
+						</Link>
+						
+						<Link className="button" to={"/user/" + this.state.leader.id + "/team/learningplan"}>
+							<div className="w100 margin-vertical-16">
+								<FontAwesomeIcon icon={faColumns} size="4x" />
+							</div>
+							{languageService.translate("Team.LearningPlan")}
 						</Link>
 						
 					</div>
